15fd3c95febc5c17b6b6e690e1b8783f1beafc6e
Sara Oliveira Pereira <sara.pereira2.external@enel.com>
2025-10-25 09:05:32 -0300

    Atualizações 25/10

--- teste/views.py
+++ teste/views.py
@@ -14,16 +14,22 @@ class ProfissionalCreateView(generics.CreateAPIView):
     permission_classes = [AllowAny]  # qualquer pessoa pode se cadastrar
 
 
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+#faz a conexão com o html
+def login_view(request): #o request puxa
+    if request.method == 'POST': # metodo post: é para adicionar dados
+        username = request.POST.get('username') # tratá do html os dados que foram inseridos no 'username' para o username
+        password = request.POST.get('password') #traz os dados que foram inseridos no 'password' para o password
+
+        user = authenticate(request, username=username, password=password) # confere se as informações são iguais
+        if user is not None: # se forem iguais
             login(request, user)
             return redirect('home')  # redireciona para a página inicial
-        else:
+        else: # se estiver incorreto, irá barrar
             messages.error(request, 'Usuário ou senha incorretos')
 
     return render(request, 'teste/login.html')
+
+# Página inicial
+def home_view(request):
+    return render(request, 'teste/home.html')
--- vagali/urls.py
+++ vagali/urls.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
-from teste.views import ProfissionalCreateView , login_view
+from teste.views import ProfissionalCreateView , login_view, home_view
 from rest_framework.authtoken import views as drf_authtoken_views
+from django.views.generic import RedirectView
 
+
+
+#cadastrar a url responsável por abrir a aba correspondente
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('cadastro/', ProfissionalCreateView.as_view(), name='cadastro'),
     path('api-token-auth/', drf_authtoken_views.obtain_auth_token, name='api_token_auth'),
-     path('login/', login_view, name='login'),
+    path('login/', login_view, name='login'),
+    path('home/', home_view, name='home'),  # página inicial
+     path('', RedirectView.as_view(url='/login/')),  # raiz redireciona para login
+
 ]
+
+
 
